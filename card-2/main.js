8aa0ccff10ebfb1bedcd22b4fe3496dc410e8330
Arthur <arthurvr723@gmail.com>
2026-05-27 22:02:05 -0300

    Adiciona logica JavaScript

---
+++ card-2/main.js
@@ -0,0 +1,12 @@
+function mostrarTabuada() {
+
+    let numero = Number(document.getElementById("numero").value);
+
+    let resultado = "";
+
+    for (let i = 1; i <= 10; i++) {
+        resultado += numero + " x " + i + " = " + (numero * i) + "<br>";
+    }
+
+    document.getElementById("resultado").innerHTML = resultado;
+}
