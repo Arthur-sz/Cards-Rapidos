c8537891ea975673c290c4813e750609682aacb4
Arthur <arthurvr723@gmail.com>
2026-05-27 22:23:44 -0300

    Adiciona card-6

---
+++ card-6/main.js
@@ -0,0 +1,13 @@
+function mostrarPares() {
+
+    let resultado = "";
+
+    for (let i = 1; i <= 50; i++) {
+
+        if (i % 2 == 0) {
+            resultado += i + " ";
+        }
+    }
+
+    document.getElementById("resultado").innerHTML = resultado;
+}
